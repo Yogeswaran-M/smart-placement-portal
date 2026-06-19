a2297cddf90ea77c623222d342646b4daae8d0e3
Yogeswaran-M <yogeglceo@gmail.com>
2026-06-19 23:07:37 +0530

    Fixed upload path and vercel routing

--- backend/middlewares/uploadResume.js
+++ backend/middlewares/uploadResume.js
@@ -1,11 +1,17 @@
 import multer from 'multer';
 import path from 'path';
+import fs from 'fs';
 
 const storage = multer.diskStorage({
     destination:(req, file, cb) => {
-        cb(null, path.join(process.cwd(), "uploads", "resumes"));
+        const uploadPath = path.join(process.cwd(), "uploads", "resumes");
         console.log(path.join(process.cwd(), "uploads", "resumes"));
-        
+
+        fs.mkdirSync(uploadPath,{
+            recursive:true
+        });
+
+        cb(null, uploadPath);
     },
     filename:(req, file, cb)=> {
         cb(null, Date.now() + "-" + file.originalname.replace(/\s+/g, "-"));
